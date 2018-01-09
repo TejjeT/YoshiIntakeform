a4c0644c73233bc6d5ddbc35125e3664f3668254
Tejeshwar Gurram <tej_jet@yahoo.com>
2018-01-09 03:01:51 -0500

    Update TeradataConfig.js

--- config/TeradataConfig.js
+++ config/TeradataConfig.js
@@ -2,12 +2,12 @@
     libpath: 'terajdbc4.jar',
     libs: ['tdgssconfig.jar'],
     drivername: 'com.teradata.jdbc.TeraDriver',
-    url: 'jdbc:teradata://oneview.kdc.capitalone.com/TMODE=TERA',
+    url: 'jdbc:teradata://terdata/TMODE=TERA',
     minpoolsize: 5,
     maxpoolsize: 10,
     // optionally
     properties: {
-        user: 'vlq927',
+        user: '',
         password: ''
     }
 };
